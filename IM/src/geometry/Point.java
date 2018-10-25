300fcf1a655b19b89e873f38eeb046d2bc229aa6
markobojkic <marko.bojkic@uns.ac.rs>
2018-10-25 10:28:00 +0200

    Vezbe 2

---
+++ IM/src/geometry/Point.java
@@ -0,0 +1,34 @@
+package geometry;
+
+public class Point {
+
+	private int x;
+	private int y;
+	private boolean selected;
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public boolean getSelected() {
+		return this.selected;
+	}
+	
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	
+	
+}
